e283e70380387d8ce3cac595d70f2cee0f29c7f9
Samuel Pereira <gmr.quest.10@gmail.com>
2023-07-02 17:49:45 -0300

    chore: create custom theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
+import { ThemeProvider } from 'styled-components';
+
 import '@/styles/globals.css';
+import { theme } from '@/styles/theme';
 
 import { Poppins } from 'next/font/google';
 
@@ -24,7 +27,9 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
 
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </div>
   );
 }
