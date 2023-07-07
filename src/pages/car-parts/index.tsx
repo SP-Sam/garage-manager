7e9c7b3fa2c0dd0aeac3614115da6f453f8fb6b1
Samuel Pereira <gmr.quest.10@gmail.com>
2023-07-07 10:02:50 -0300

    feat: render store data on the page

--- src/pages/car-parts/index.tsx
+++ src/pages/car-parts/index.tsx
@@ -7,8 +7,19 @@ import { CarPartListContainer, CarPartsContainer, NoPartsText } from './styles';
 
 import Layout from '@/components/layout';
 import CarPartCard from '@/components/CarPartCard';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '@/store';
+import { useEffect } from 'react';
+import { fetchCarParts } from '@/store/carParts';
 
 const CarParts: NextPage = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const { data: carParts } = useSelector((state: RootState) => state.carParts);
+
+  useEffect(() => {
+    dispatch(fetchCarParts({}));
+  }, [dispatch]);
+
   return (
     <Layout pageTitle="Peças">
       <Head>
@@ -18,11 +29,14 @@ const CarParts: NextPage = () => {
       <CarPartsContainer>
         <NoPartsText>Não há peças cadastradas ainda</NoPartsText>
         <CarPartListContainer>
-          <CarPartCard
-            id={1}
-            name="Amortecedor dianteiro Amortecedor dianteiro dianteiro Amortecedor dianteiro"
-            price={999999.99}
-          />
+          {carParts.map((carPart) => (
+            <CarPartCard
+              key={carPart.id}
+              id={carPart.id}
+              name={carPart.name}
+              price={carPart.price}
+            />
+          ))}
         </CarPartListContainer>
       </CarPartsContainer>
     </Layout>
